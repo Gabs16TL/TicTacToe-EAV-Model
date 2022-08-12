edc804ff5897055c00a5f83e6bf79be99be8e444
Gabriela <gabi16.hernandez@gmail.com>
2022-08-12 10:44:47 -0600

    Querys solicitados

---
+++ DML/Querys_per_item.sql
@@ -0,0 +1,22 @@
+/*Jugadores que han ganado al menos partida*/
+
+
+
+
+/*Jugadores que han perdido al menos una partida*/
+
+
+
+
+/*Jugadores que no han jugado una partida*/
+
+
+
+/*Como quedó el tablero del mejor jugador*/
+
+
+
+/*Como quedó el tablero del peor jugador*/
+
+
+    
